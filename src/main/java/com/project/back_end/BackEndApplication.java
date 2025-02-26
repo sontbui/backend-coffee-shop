227f5c0223150db250c027a47807e18a9d38d107
sontbui <sonbuithanh306@gmail.com>
2025-02-26 12:22:22 +0700

    Fix secret key

--- src/main/java/com/project/back_end/BackEndApplication.java
+++ src/main/java/com/project/back_end/BackEndApplication.java
@@ -1,34 +1,33 @@
 package com.project.back_end;
 
-import com.project.back_end.services.MongoDBConnectionService;
-
 import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.security.Keys;
-
 import java.util.Base64;
 
 import javax.crypto.SecretKey;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @EnableMongoRepositories(basePackages = "com.project.back_end.repositories")
 public class BackEndApplication {
 
-   
     public static void main(String[] args) {
-        SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS512);
-        String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
-        System.out.println("New JWT Secret Key: " + base64Key);
         Dotenv dotenv = Dotenv.load();
         System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
         System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
         System.setProperty("MONGO_DATABASE", dotenv.get("MONGO_DATABASE"));
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
+        String jwtSecret = System.getenv("JWT_SECRET");
+
+        if (jwtSecret == null || jwtSecret.isEmpty()) {
+            SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS512);
+            String base64Key = Base64.getEncoder().encodeToString(key.getEncoded());
+            System.out.println("⚠️ New JWT Secret Key (Only for local development): " + base64Key);
+        }
         SpringApplication.run(BackEndApplication.class, args);
     }
 }
